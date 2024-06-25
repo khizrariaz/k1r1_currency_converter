ca82be33b352af17d8415d2f58fc87a71673749e
Khizra Riaz <khizrariaz@gmail.com>
2024-06-26 00:06:49 +0500

    SIMPLIFIED

--- main.ts
+++ main.ts
@@ -1,52 +1,27 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 
-let converstion = {
-"PKR": {
-    "USD": 0.0044,
-    "GBP": 0.0037,
-    "PKR": 1
-},
-"GBP": {
-    "USD": 1.21,
-    "PKR" : 271.79,
-    "GBP": 1
-},
-"USD": {
-    "PKR": 225.50,
-    "GBP": 0.83,
-    "USD": 1
+let c_value : {[key: string]: number} = {
+    PKR: 278.40,
+    UAE: 3.67,
+    USD: 1
 }
-}
-
-const answer: {
-    from: "PKR"|"GBP" | "USD",
-    to: "PKR"|"GBP" | "USD",
-    amount: number
-} = await inquirer.prompt([
-    {
+let answer = await inquirer.prompt([{
         type: "list",
         name: "from",
-        choices: ["PKR", "GBP", "USD"],
-        message: "Select your currency:"
-    },
-    {
+        choices: ["PKR", "UAE", "USD"],
+        message: "Converting from: "
+},
+{
         type: "list",
         name: "to",
-        choices: ["PKR", "GBP", "USD"],
-        message: "Select your convertion currency:"
-    },
-    {
+        choices: ["PKR", "UAE", "USD"],
+        message: "Converting to: "
+},
+{
         type: "number",
         name: "amount",
         message: "Enter your convertion amount:"
-    }
-]);
+}]);
 
-const { from, to, amount } = answer;
-if (from && to && amount) { 
-   let result = converstion[from][to] * amount;
-   console.log(`Your convertion from ${from} to ${to} is ${result}`);
-}else {
-    console.log(" Invalid inputs");
-}
+console.log(c_value [answer.to] / c_value [answer.from] * answer.amount);
